296908d31a9f62a0a1bb1a290aa4b47fb6162ff3
asyifaauliarahma08 <asyifaauliarahmaasyifa@gmail.com>
2025-01-15 14:00:12 +0700

    laravel library

--- app/Models/Reviews.php
+++ app/Models/Reviews.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reviews extends Model
 {
-    //
+    // Nama tabel (opsional)
+    protected $table = 'reviews';
+
+    // Kolom yang dapat diisi
+    protected $fillable = [
+        'book_id',
+        'user_id',
+        'rating',
+        'comment',
+        'created_at',
+        'updated_at',
+    ];
+
+    // Relasi ke tabel Books
+    public function book()
+    {
+        return $this->belongsTo(Book::class, 'book_id');
+    }
+
+    // Relasi ke tabel Users
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
--- database/migrations/2025_01_15_061404_create_categories_table.php
+++ database/migrations/2025_01_15_061404_create_categories_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string("name");
+            $table->name();
             $table->timestamps();
         });
     }
@@ -24,5 +24,8 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('categories');
+        $table->id();
+            $table->string("name");
+            $table->timestamps();
     }
 };
